da00592a263769e2cfbd09b9131af4f2bd35e43a
himuuuuuuuuu <himanshu92jain@gmail.com>
2025-04-03 18:10:36 +0530

    variable

---
+++ Day-2 Variables&datatypes/JavaBasics.java
@@ -0,0 +1,17 @@
+public class JavaBasics {
+    public static void main(String args[]) {
+        System.out.print("Hello World\n");//line ka space
+        System.out.print("Hello World\n");//line ka space
+        System.out.print("Hello World\n");// line ka space
+    }
+}
+
+
+
+//boilerplate code
+//Output in Java
+// 1.print  :System.out.print("Hello World"); -double quote represents string
+
+// 2.println :System.out.println("Hello World");
+
+// 3."\n"
---
+++ Day-2 Variables&datatypes/variables.java
@@ -0,0 +1,16 @@
+public class variables {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a );
+        System.out.println(b);
+        String name ="Tony Stark";
+        System.out.println(name);
+
+            // a = 20;
+            a = b;
+            System.out.println(a);
+
+    }
+    
+}
